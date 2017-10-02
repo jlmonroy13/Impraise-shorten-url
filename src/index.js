924f92715d52283f1f39d49018eb73d609932c14
jlmonroy13 <jlmonroy13@gmail.com>
2017-10-02 02:28:46 -0500

    Markup, styles and redux store

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
-import App from './components/App';
+import App from './components/App/';
+import './assets/stylesheets/main.scss';
 
 const store = configureStore();
 
